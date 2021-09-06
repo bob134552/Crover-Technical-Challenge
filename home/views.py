cff16f534cc7ccba9607877e89c7495ca78e9239
bob134552 <bob134552@hotmail.com>
2021-09-06 18:35:20 +0100

    [3] Correctly displays data
    - Added selector to switch to different data sets.

--- home/views.py
+++ home/views.py
@@ -1,19 +1,73 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
 from django.contrib import messages
 from .models import TempData
-
+from plotly.offline import plot
+import plotly.graph_objs as go
 import io, csv
+import pandas as pd
+import plotly.express as px
+from ordered_set import OrderedSet
 
 
 def home(request):
     data = TempData.objects.all()
-    sets = [item.set for item in data]
+    sets = OrderedSet([item.set for item in data])
+    current_set = sets[0]
+    if 'new_set' in request.GET:
+        new_set = request.GET['new_set']
+        current_set = new_set
+    current_data = data.filter(set=current_set)
+    x_data = []
+    y_data = []
+    temperatures = []
+
+    for x in current_data:
+        x_data.append(x.x_point)
+        y_data.append(x.y_point)
+        temperatures.append(x.temperature)
+    bottom_axis = list(range(0, len(temperatures)))
+    
+
+    df = pd.DataFrame({'temperatures': temperatures,
+                        'bottom_axis': bottom_axis,
+                        'x_data': x_data,
+                        'y_data': y_data})
+
+
+    scatter = px.scatter(df, x='bottom_axis', y='temperatures',
+                         hover_data={"temperatures": True,
+                                     "x_data": True, "y_data": True,
+                                     "bottom_axis": True
+                                    },
+                         labels={
+                             'bottom_axis': 'Data Point',
+                             'temperatures': "Temperature(°C)",
+                             'x_data': "Position X(m)",
+                             'y_data': "Position Y(m)"
+                         })
+    config = dict({'scrollZoom': True})
+    layout = {
+        "title":{
+            'text': f"Silo Temperature: {current_set}",
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        'xaxis_title': '',
+        'yaxis_title': 'Temperature(°C)',
+        'height': 800,
+    }
+    scatter.update_layout(layout)
+    scatter.update_traces(mode="markers+lines",marker=dict(color='#5D69B1', size=8), line=dict(color='#52BCA3', width=1))
+
+    fig = go.Figure(data=scatter, layout=layout)
+    graph = plot(fig, output_type='div', include_plotlyjs=False, config=config)
+
     context = {
-        'prompt': 'Order of CSV should be x, y and temperature and filename.',
-        'data': data    
+        'sets': sets,
+        'selected_set': current_set,
+        'graph': graph,
         }
-    # TempData.objects.all().delete()
     if request.method == "POST":
         # declaring template
         csv_file = request.FILES['file']
@@ -35,7 +89,8 @@ def home(request):
         try:
             TempData.objects.bulk_create(objs)
             messages.success(request, "Import complete")
+            return redirect('home')
         except Exception as e:
-            messages.error(request, "Failed To import")
+            messages.error(request, f"Failed To import, Error: {e}")
             return redirect('home')
     return render(request, 'home/index.html', context)
